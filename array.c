478ead939e987c123bbe3a745fe9e06d8b5a93d9
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-01-26 23:54:55 +0600

    goo

---
+++ array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main (){
+int a [40];
+int i;
+for (i=0;i<40;i++)
+{
+  a[i]=i+1;
+  printf("a[%d]-->%d\n",i,a[i]);
+}
+ 
+  return 0;
+
+}
+
+
+/*#include<stdio.h>
+int main (){
+int a [40];
+int i;
+for (i=40;i>0;i++)
+{
+  a[i]=i+1;
+  printf("a[%d]-->%d\n",i,a[i]);
+}
+ 
+  return 0;
+
+}*/
+
